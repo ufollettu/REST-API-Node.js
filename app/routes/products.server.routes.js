3a98d6c1c17fc51bd510299d709003b3787ce20d
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-02-12 14:43:24 +0100

    added validation

--- app/routes/products.server.routes.js
+++ app/routes/products.server.routes.js
@@ -6,12 +6,27 @@ const products = require("../controllers/products.server.controller");
 
 router.get("/", (req, res, next) => {
     Product.find()
+        .select("name price _id") //select the only fields you want to get
         .exec()
         .then(docs => {
-            console.log(docs);
+            const response = {
+                count: docs.length,
+              products: docs.map(doc => {
+                  return {
+                      name: doc.name,
+                      price: doc.price,
+                      _id: doc._id,
+                      request: {
+                          type: "GET",
+                          url: "http://localhost:3000/products/" + doc._id
+                      }
+                  }
+              })
+            };
+            // console.log(docs);
             //Wrap in if to manage no entries database response. Uncomment to do that
             // if (docs.length >= 0) {
-            res.status(200).json(docs);
+            res.status(200).json(response);
             // } else {
             //     res.status(404).json({
             //         message: "no entries found"
@@ -34,8 +49,16 @@ router.post("/", (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: "post req to /products",
-                createdProduct: result
+                message: "Created product",
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: "POST",
+                        url: "http://localhost:3000/products/" + result._id
+                    }
+                }
             });
         })
         .catch(err => {
@@ -47,11 +70,18 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select("name price _id") //select the only fields you want to get
         .exec()
         .then(doc => {
             console.log("from db:", doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/products/" + doc._id
+                    }
+                });
             } else {
                 res.status(404).json({message: "no valid entry found for provided ID"});
             }
@@ -71,9 +101,14 @@ router.patch("/:productId", (req, res, next) => {
     }
     Product.update({_id: id}, {$set: updateOps})
         .exec()
-        .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+        .then(() => {
+            res.status(200).json({
+                message: "product updated",
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/products/" + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -85,8 +120,15 @@ router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.remove({_id: id})
         .exec()
-        .then(result => {
-            res.status(200).json(result);
+        .then(() => {
+            res.status(200).json({
+                message: "product deleted",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:3000/products/" + id,
+                    body: {name: "String", price: "Number"}
+                }
+            });
         })
         .catch(err => {
             console.log(err);
